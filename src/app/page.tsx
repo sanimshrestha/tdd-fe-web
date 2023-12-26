1e70d269e40e408b1d5fd348a385be44957dd0a6
bhujels <bhujelsampada@gmail.com>
2023-12-26 15:30:11 -0500

    Next.js Setup

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import { constants } from '@/utils/constants'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex flex-col items-center justify-center">
+        <Image
+          src="/logo.svg"
+          alt= {constants.productTitle}
+          width={200}
+          height={200}
+        />
+        <h1 className="text-4xl font-bold">The Drinks Diary</h1>
+        <p className="text-xl">For cocktails and other drinks</p>
+      </div>
+    </main>
+  )
+}
---
+++ src/utils/constants.ts
@@ -0,0 +1,7 @@
+export const constants = {
+  productTitle : "The Drinks Diary",
+  productDescription : "For cocktails and other drinks"
+}
+
+
+export type ConstantsType = typeof constants
