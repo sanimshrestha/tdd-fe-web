46f39ba72bf2427286b52e8aefc9ee2954042cde
bhujels <bhujelsampada@gmail.com>
2024-01-09 22:31:23 -0500

    Add clarity script to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Lexend } from 'next/font/google'
 import './globals.css'
 import { constants } from './lib/constants'
+import Script from 'next/script'
 
 const lexend = Lexend({
   subsets: ['latin'],
@@ -22,6 +23,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className='h-full'>
+      {/* Clarity Script */}
+      <Script strategy="lazyOnload" id="clarity-script">
+        {`
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "kj9efzwzc3");
+        `}
+      </Script>
+
       <body className={`${lexend.variable} font-sans h-full`}>
         {children}
       </body>
