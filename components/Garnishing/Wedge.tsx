faa9ccd4e82f9de8973917e79c6129485b808000
bhujels <bhujelsampada@gmail.com>
2024-03-18 17:42:53 -0400

    made default animation spawn for all garnishings:

--- components/Garnishing/Wedge.tsx
+++ components/Garnishing/Wedge.tsx
@@ -6,27 +6,7 @@ import {
   defaultGarnishingPlacements,
   garnishingPlacements
 } from "./garnishingPlacements";
-
-const animationStages = [
-  {
-    opacity: 0,
-    x: '-25%',
-    y: '135%',
-    scale: 0.1,
-    rotate: -45,
-  },
-  {
-    opacity: 1,
-    x: '0%',
-    y: '0%',
-    scale: 1,
-    rotate: 10,
-  },
-  {
-    rotate: 0,
-  }
-]
-
+import animationStages from "./animations";
 
 const Wedge = (props: GarnishingProps) => {
   const { animate: showAnimation, garnishing, glass } = props;
@@ -42,7 +22,8 @@ const Wedge = (props: GarnishingProps) => {
   useEffect(() => {
     if (!showAnimation) return;
     (async () => {
-      await animate(scope.current, animationStages[1], { duration: 0.4 });
+      await animate(scope.current, animationStages[1],
+        { duration: 0.3, delay: 0.3 });
 
       animate(scope.current, animationStages[2], {
         type: 'spring',
@@ -67,8 +48,6 @@ const Wedge = (props: GarnishingProps) => {
 
   return (
     <motion.div
-      ref={scope}
-      initial={showAnimation ? animationStages[0] : animationStages[2]}
       style={{ top: `${topPos}%`, left: `${leftPos}%` }}
       className="absolute pointer-events-none w-[24.6%] z-10" //124/504 = 24.6%
     >
@@ -78,7 +57,10 @@ const Wedge = (props: GarnishingProps) => {
             `translate(${translateX || "-50%"}, ${translateY || "-50%"}) 
             rotate(${rotation}deg)`
         }}>
-        <svg width="100%" viewBox="0 0 140 248" xmlns="http://www.w3.org/2000/svg">
+        <motion.svg width="100%" viewBox="0 0 140 248" xmlns="http://www.w3.org/2000/svg"
+          ref={scope}
+          initial={showAnimation ? animationStages[0] : animationStages[2]}
+        >
           <path d="M16 248C32.9397 248 49.6998 244.529 65.2463 237.802
           C80.7928 231.074 94.7961 221.232 106.392 208.884
           C117.988 196.535 126.931 181.942 132.669 166.004
@@ -92,7 +74,7 @@ const Wedge = (props: GarnishingProps) => {
             stroke={strokeColor}
             strokeWidth={strokeWidth}
           />
-        </svg>
+        </motion.svg>
       </div>
     </motion.div >
   );
